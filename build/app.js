e6e7eb39c4cbc9a99850df6df8b60d5766148910
DennisWanjiru <denniswanjiru71@gmail.com>
2017-11-04 18:17:51 +0300

    Arrays

--- build/app.js
+++ build/app.js
@@ -56,28 +56,27 @@ let obj = {
 
 console.log(JSON.stringify(obj));
 
-*/
-
 //METHOD PROPERTIES
-var myObj = {
-    add: function add(a, b) {
+let myObj = {
+    add(a, b) {
         return a + b;
     },
-    multiply: function multiply(a, b) {
+
+    multiply(a, b) {
         return a * b;
     }
-};
+}
 
-var b = myObj.multiply(2, 6);
+let b = myObj.multiply(2, 6);
 console.log(b);
 
 //OBJECT ASSIGN METHOD
-var obj = {
+let obj = {
     fname: "Dennis",
     lname: "Wanjiru"
 };
 
-var obj2 = {
+let obj2 = {
     jina: "Chrus",
 
     get lapili() {
@@ -85,12 +84,85 @@ var obj2 = {
     }
 };
 
-var obj3 = {
+let obj3 = {
     namer: "Ken",
     laname: "Milo"
 };
 
-var copy = Object.assign({}, obj, obj2, obj3);
+let copy = Object.assign({}, obj, obj2, obj3);
 
 console.log(copy);
 console.log(obj);
+
+//MATH
+console.log(Math.trunc(12.34)); //Removes the decimal part of x and returns the integral part.
+console.log(Math.cbrt(256)); //Returns the cube root of x.
+
+let k = "12.12";
+let p = Number.parseFloat(k);
+console.log(typeof p);
+console.log(typeof k);
+
+*/
+
+//ARRAY
+var arr = Array.of("Dennis", 23, "Male");
+console.log(arr);
+
+var eArr = arr.entries();
+
+for (var i = 0; i < arr.length; i++) {
+    console.log(eArr.next().value);
+}
+
+//COPY
+console.log([1, 2, 3, 4, 5].copyWithin(0, 3));
+
+//FILL
+console.log([1, 2, 3, 4, 6].fill(5, 2, 4));
+
+//Includes
+console.log([1.2, 3, 4, 5].includes(3));
+console.log([1.2, 3, 4, 5].includes(4, 2));
+console.log([1.2, 3, 4, 5].includes(4, 4));
+console.log([1.2, 3, 4, 5].includes(8));
+
+var kArr = arr.keys();
+
+for (var _i = 0; _i < arr.length; _i++) {
+    console.log(kArr.next());
+}
+
+var arr2 = ["wanjiru", "elly", "tim"];
+
+// var vArr = arr2.values();
+
+// for (let letter of vArr) {
+//     console.log(letter);
+// }
+
+var names = arr2[Symbol.iterator]();
+var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+    for (var _iterator = names[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var name = _step.value;
+
+        console.log(name);
+    }
+} catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+} finally {
+    try {
+        if (!_iteratorNormalCompletion && _iterator.return) {
+            _iterator.return();
+        }
+    } finally {
+        if (_didIteratorError) {
+            throw _iteratorError;
+        }
+    }
+}
--- src/app.js
+++ src/app.js
@@ -54,8 +54,6 @@ let obj = {
 
 console.log(JSON.stringify(obj));
 
-*/
-
 //METHOD PROPERTIES
 let myObj = {
     add(a, b) {
@@ -93,3 +91,57 @@ let copy = Object.assign({}, obj, obj2, obj3);
 
 console.log(copy);
 console.log(obj);
+
+//MATH
+console.log(Math.trunc(12.34)); //Removes the decimal part of x and returns the integral part.
+console.log(Math.cbrt(256)); //Returns the cube root of x.
+
+let k = "12.12";
+let p = Number.parseFloat(k);
+console.log(typeof p);
+console.log(typeof k);
+
+*/
+
+//ARRAY
+let arr = Array.of("Dennis", 23, "Male");
+console.log(arr)
+
+let eArr = arr.entries();
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(eArr.next().value)
+}
+
+//COPY
+console.log([1, 2, 3, 4, 5].copyWithin(0, 3));
+
+//FILL
+console.log([1, 2, 3, 4, 6].fill(5, 2, 4))
+
+//Includes
+console.log([1.2, 3, 4, 5].includes(3))
+console.log([1.2, 3, 4, 5].includes(4, 2))
+console.log([1.2, 3, 4, 5].includes(4, 4))
+console.log([1.2, 3, 4, 5].includes(8))
+
+let kArr = arr.keys();
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(kArr.next())
+}
+
+//Symbol Iterator
+
+var arr2 = ["wanjiru", "elly", "tim"];
+
+// var vArr = arr2.values();
+
+// for (let letter of vArr) {
+//     console.log(letter);
+// }
+
+let names = arr2[Symbol.iterator]();
+for (let name of names) {
+    console.log(name);
+}
