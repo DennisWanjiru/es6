af503af5ce4d5c6ee79021a0c7fabd26eef05a93
DennisWanjiru <denniswanjiru71@gmail.com>
2017-11-04 11:08:10 +0300

    Object creation in es6

--- build/app.js
+++ build/app.js
@@ -1,7 +1,37 @@
 "use strict";
 
-var myFunc = function myFunc() {
-  return console.log("Hello World");
-};
+//THE LET KEYWORD WITH LEXICAL BLOCK SCOPING
+/*
+let shoppingCart = [{
+    id: 0,
+    product: "DVD",
+    price: 12.56
+}, {
+    id: 1,
+    product: "CD",
+    price: 9.56
+}, {
+    id: 2,
+    product: "MP3",
+    price: 2.56
+}];
 
-myFunc();
+for (let i = 0; i < shoppingCart.length; i++) {
+    let item = shoppingCart[i];
+    console.log("Item: " + item.product + " - Price: " + item.price)
+}
+
+*/
+
+//OBJECTS CREATION IN ES6
+function createDog(name, ability) {
+    return {
+        type: "animal",
+        name: name,
+        ability: ability
+    };
+}
+
+var a = createDog("Wolf", "Bark");
+
+console.log(JSON.stringify(a));
--- src/app.js
+++ src/app.js
@@ -1,3 +1,35 @@
-let myFunc = () => console.log("Hello World");
+//THE LET KEYWORD WITH LEXICAL BLOCK SCOPING
+/*
+let shoppingCart = [{
+    id: 0,
+    product: "DVD",
+    price: 12.56
+}, {
+    id: 1,
+    product: "CD",
+    price: 9.56
+}, {
+    id: 2,
+    product: "MP3",
+    price: 2.56
+}];
 
-myFunc();
+for (let i = 0; i < shoppingCart.length; i++) {
+    let item = shoppingCart[i];
+    console.log("Item: " + item.product + " - Price: " + item.price)
+}
+
+*/
+
+//OBJECTS CREATION IN ES6
+function createDog(name, ability) {
+    return {
+        type: "animal",
+        name,
+        ability
+    }
+}
+
+let a = createDog("Wolf", "Bark");
+
+console.log(JSON.stringify(a));
